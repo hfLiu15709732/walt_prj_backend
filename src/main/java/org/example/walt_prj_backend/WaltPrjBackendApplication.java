a1651b68283685b9d9bcdb3cece4e8a56917fbab
hfLiuX <hfliu15709732@163.com>
2025-08-16 17:27:20 +0800

    feat: 项目初始化

---
+++ src/main/java/org/example/walt_prj_backend/WaltPrjBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.walt_prj_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaltPrjBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaltPrjBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/walt_prj_backend/WaltPrjBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.walt_prj_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaltPrjBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
